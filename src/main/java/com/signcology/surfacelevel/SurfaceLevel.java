f4b9c17239b134c01a9189425396f4388939a245
Signcology <plalaw48@gmail.com>
2025-10-25 07:32:45 -0400

    Removed unused code !!

--- src/main/java/com/signcology/surfacelevel/SurfaceLevel.java
+++ src/main/java/com/signcology/surfacelevel/SurfaceLevel.java
@@ -5,8 +5,6 @@ import com.signcology.surfacelevel.block.ModBlocks;
 import com.signcology.surfacelevel.item.ModCreativeModeTabs;
 import com.signcology.surfacelevel.item.ModItems;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
@@ -21,7 +19,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
