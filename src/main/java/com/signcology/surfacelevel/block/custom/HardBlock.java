becd16e9707c2df1c9bb07cd0244db6759d94588
Signcology <plalaw48@gmail.com>
2025-10-25 23:32:14 -0400

    Removed unused code

--- src/main/java/com/signcology/surfacelevel/block/custom/HardBlock.java
+++ src/main/java/com/signcology/surfacelevel/block/custom/HardBlock.java
@@ -2,9 +2,6 @@ package com.signcology.surfacelevel.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
