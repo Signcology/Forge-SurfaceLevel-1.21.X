1204a80440e57995b6b347f5b0316f5e1b01eb8f
Signcology <plalaw48@gmail.com>
2025-10-27 04:40:32 -0400

    Removed unused imports

--- src/main/java/com/signcology/surfacelevel/SurfaceLevel.java
+++ src/main/java/com/signcology/surfacelevel/SurfaceLevel.java
@@ -6,7 +6,6 @@ import com.signcology.surfacelevel.item.ModCreativeModeTabs;
 import com.signcology.surfacelevel.item.ModItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -19,7 +18,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
